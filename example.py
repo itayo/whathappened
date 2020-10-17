b249519947c2470fa37b224d71c6575fd16ab23c
Rollcloud <rollcloud@gmail.com>
2020-10-17 17:26:01 +0200

    feat: create example.py

---
+++ example.py
@@ -0,0 +1,17 @@
+from whathappened import changelog as cl
+from whathappened.git_commits import get_commits
+
+
+def main():
+    start = 'v0.0.0'
+    end = None
+    commits = get_commits()
+    commits = cl.filter_commits(commits, start, end)
+    versions = cl.compile_log(commits)
+    log = cl.format_log(versions)
+    cl.write_log(log, "CHANGELOG.md")
+
+
+if __name__ == '__main__':
+
+    main()
